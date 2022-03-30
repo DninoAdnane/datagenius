c674c677e0ff167b4bc0cc19474f1c0ef2b5ad72
DninoAdnane <da_guettaf@esi.dz>
2022-03-30 16:55:30 +0200

    ajouter du listing des produit par rapport au rayon et interval de prix

--- product_api/urls.py
+++ product_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from product_api.views import product_list
+from product_api.views import product_list, product_select, product_rayon, product_prix
 
 urlpatterns = [
     path('product/', product_list),
+    path('product_select/<str:name>/', product_select),
+    path('product_rayon/<str:rayon>/', product_rayon),
+    path('product_prix', product_prix)
 ]
--- product_api/views.py
+++ product_api/views.py
@@ -4,18 +4,65 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from product_api.models import Product
 from product_api.serializers import ProductSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+
+@api_view(['GET'])
 def product_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
         productSerializer =  ProductSerializer(products, many = True)
-        return JsonResponse(productSerializer.data, safe = False)
+        return Response(productSerializer.data)
     elif request.method == 'POST':
         pass
-        # data = JSONParser().parse(request)
-        # prodSerialize = ProductSerializer(data = data)
-
+        # prodSerialize = ProductSerializer(data = request.data)
         # if prodSerialize.is_valid():
         #     prodSerialize.save()
-        #     return JsonResponse(prodSerialize.data, status = 201)
+        #     return Response(prodSerialize.data, status = 201)
         # return JsonResponse(prodSerialize.errors, status = 400)
+
+
+def product_list_name(request):
+    if request.method == 'GET':
+        Product.objects.all()
+
+@api_view(['GET'])
+def product_select(request, name):
+    try:
+        product = Product.objects.get(name=name)
+    except Product.DoesNotExist:
+        return Response("Product not available...", status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        productSerializer =  ProductSerializer(product)
+        return Response(productSerializer.data)
+
+@api_view(['GET'])
+def product_rayon(request, rayon):
+    try:
+        product = Product.objects.get(rayon = rayon)
+    except Product.DoesNotExist:
+        return Response("No products available in this current rayon....", status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        productSerializer =  ProductSerializer(product)
+        return Response(productSerializer.data)
+
+@api_view(['POST'])
+def product_prix(request):
+    try:
+        min = request.data.get('min')
+        max = request.data.get('max')
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    product = Product.objects.filter(prix__gte = min, prix__lte = max)
+    if request.method == 'POST':
+        productSerializer =  ProductSerializer(product, many = True)
+        if len(product) == 0: 
+            return Response({"message": "No products available in this range of prices....", "data": productSerializer.data}  , status = status.HTTP_404_NOT_FOUND)
+        return Response(productSerializer.data)
+
+    
